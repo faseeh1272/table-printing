72ed22690626519f842e8f79c34c7511bd2046f4
faseeh1272 <syedfaseehnaqvi2@gmail.com>
2024-09-30 14:50:05 -0700

    change

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var userValue = +prompt("enter a number and generate its table:");
 
-for (var i = 1; i<=userValue; userValue--) {
+for (var i = 1; i<=userValue; userValue++) {
 
     for (var j = 1; j<=10; j++){
         document.write("<br>", userValue + " x " + j + "=" + userValue * j)
